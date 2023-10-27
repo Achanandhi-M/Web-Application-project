a6e255ef8750ccec3f5fadb212389c257317c571
Achanandhi-M <achanandhi.m@gmail.com>
2023-10-27 23:00:01 +0530

    Modified-file

---
+++ index.js
@@ -0,0 +1,20 @@
+const quotes = [
+    { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+    { text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill" },
+    { text: "In the middle of every difficulty lies opportunity.", author: "Albert Einstein" },
+    { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" }
+];
+
+const quoteElement = document.querySelector('.quote');
+const authorElement = document.querySelector('.author');
+
+let currentQuoteIndex = 0;
+
+function displayQuote() {
+    quoteElement.textContent = quotes[currentQuoteIndex].text;
+    authorElement.textContent = `- ${quotes[currentQuoteIndex].author}`;
+    currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
+}
+
+displayQuote();
+setInterval(displayQuote, 7000);
